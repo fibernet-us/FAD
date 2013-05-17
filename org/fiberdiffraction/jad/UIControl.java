bb5ea3100a514566fc154795569256ec152b1c5d
billz <j2eeplus@gmail.com>
2013-05-17 18:19:07 -0500

    Added DataControl class holding data items associated with UIControl Updated UIControl to add hooks between text fields and control data

--- org/fiberdiffraction/jad/UIControl.java
+++ org/fiberdiffraction/jad/UIControl.java
@@ -36,6 +36,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JComponent;
+import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -59,66 +60,7 @@ public class UIControl extends JPanel {
 	private JTabbedPane controlTabs;
 	private JPanel pageRun, pageInp, pagePar, pageBg;
 	
-	enum Jtype { BUTTON, LABEL, TFIELD };
-	
-	static class WidgetInfo {
-		Jtype type; 
-		String name;
-		WidgetInfo(Jtype type, String name) {
-			this.type = type;
-			this.name = name;
-		}
-	}
-	
-	private static WidgetInfo[] widgetsRun = {
-			new WidgetInfo(Jtype.BUTTON, "Init"),  new WidgetInfo(Jtype.BUTTON, "Batch"),
-			new WidgetInfo(Jtype.BUTTON, "CurrR"), new WidgetInfo(Jtype.TFIELD, "TcurrR"),
-			new WidgetInfo(Jtype.BUTTON, "NextR"), new WidgetInfo(Jtype.BUTTON, "PrevR"),
-			new WidgetInfo(Jtype.BUTTON, "Cut"),   new WidgetInfo(Jtype.TFIELD, "Tcut"),
-			new WidgetInfo(Jtype.BUTTON, "Uncut"), new WidgetInfo(Jtype.BUTTON, "Intgr"),
-			new WidgetInfo(Jtype.BUTTON, "Summ"),  new WidgetInfo(Jtype.BUTTON, "Help") 
-	};
-
-	private static WidgetInfo[] widgetsInp = { 
-			new WidgetInfo(Jtype.LABEL, " XWAV"),
-			new WidgetInfo(Jtype.LABEL, "  SFD"), 
-			new WidgetInfo(Jtype.LABEL, "    C"),
-			new WidgetInfo(Jtype.LABEL, "   DR"), 
-			new WidgetInfo(Jtype.LABEL, "   WR"),
-			new WidgetInfo(Jtype.LABEL, "  IFV"), 
-			new WidgetInfo(Jtype.LABEL, " NULB"),
-			new WidgetInfo(Jtype.LABEL, " RMIN"), 
-			new WidgetInfo(Jtype.LABEL, "   NR"),
-			new WidgetInfo(Jtype.LABEL, "THMIN"), 
-			new WidgetInfo(Jtype.LABEL, "DTHET"),
-			new WidgetInfo(Jtype.LABEL, "NTHET") 
-	};
-
-	private static WidgetInfo[] widgetsPar = { 
-			new WidgetInfo(Jtype.LABEL, " NREF"),
-			new WidgetInfo(Jtype.LABEL, "  NPR"), 
-			new WidgetInfo(Jtype.LABEL, "MAXLS"),
-			new WidgetInfo(Jtype.LABEL, "MINCU"), 
-			new WidgetInfo(Jtype.LABEL, " XINT"),
-			new WidgetInfo(Jtype.LABEL, " BINT"), 
-			new WidgetInfo(Jtype.LABEL, " INTL"),
-			new WidgetInfo(Jtype.LABEL, "  NBG"), 
-			new WidgetInfo(Jtype.LABEL, "  BG2"),
-			new WidgetInfo(Jtype.LABEL, "  BG3"), 
-			new WidgetInfo(Jtype.LABEL, " CENB"),
-			new WidgetInfo(Jtype.LABEL, " SIGB") 
-	};
-
-	private static WidgetInfo[] widgetsBg = { 
-			new WidgetInfo(Jtype.LABEL, "   IG"),
-			new WidgetInfo(Jtype.LABEL, " SIG1"), 
-			new WidgetInfo(Jtype.LABEL, " SIG2"),
-			new WidgetInfo(Jtype.LABEL, " DECN"), 
-			new WidgetInfo(Jtype.LABEL, " GRAT") 
-	};
-
-	// need to find widget by name to do things like setting text field
-	private Map<String, JComponent> parTable;   
+	private Map<String, DataControl.DatumDef> parTable;   
 	
 	/**
 	 * Constructor
@@ -129,7 +71,7 @@ public class UIControl extends JPanel {
 		this.height = height;
 		setPreferredSize(new Dimension(width, height)); 
 		setMinimumSize(new Dimension(width, height)); 
-		parTable = new HashMap<String, JComponent>();
+		parTable = new HashMap<String, DataControl.DatumDef>();
 		initialize();
 	}
 
@@ -141,10 +83,10 @@ public class UIControl extends JPanel {
 		setLayout(new BorderLayout());
 
 		// Create tab pages
-		createTabPage(pageRun = new JPanel(), widgetsRun);
-		createTabPage(pageInp = new JPanel(), widgetsInp);
-		createTabPage(pagePar = new JPanel(), widgetsPar);
-		createTabPage(pageBg = new JPanel(), widgetsBg);
+		createTabPage(pageRun = new JPanel(), DataControl.DataRun);
+		createTabPage(pageInp = new JPanel(), DataControl.DataInp);
+		createTabPage(pagePar = new JPanel(), DataControl.DataPar);
+		createTabPage(pageBg  = new JPanel(), DataControl.DataBg);
 
 		// Create tabs
 		controlTabs = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
@@ -156,82 +98,89 @@ public class UIControl extends JPanel {
 		add(controlTabs, BorderLayout.CENTER);
 	}
 
-	private void createTabPage(JPanel parent, WidgetInfo[] widgets) {
+	private void createTabPage(JPanel parent, DataControl.DatumDef[] widgets) {
 
+		//
+		// first create the widgets and put them into a list
+		//
+		
+		ArrayList<JComponent> jclist = new ArrayList<JComponent>();
+		JComponent jc;
+		
+		for (int i = 0; i < widgets.length; i++) {
+			
+			switch (widgets[i].getType()) {
+				case BUTTON:
+					jc = new JButton(widgets[i].getName());
+					jc.setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jc.setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jclist.add(jc);
+					break;
+					//
+				case LABELED_TFIELD:
+					jc = new JLabel(widgets[i].getName());
+					jc.setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jc.setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jclist.add(jc);
+					// fall through. label always followed by its text field
+					//
+				case TFIELD:
+					jc = new JTextField();
+					jc.setName(widgets[i].getName());				
+					jc.setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jc.setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
+					jclist.add(jc);	
+					
+					((JTextField) jc).setColumns(10);
+					((JTextField) jc).setText(widgets[i].getStringValue());
+					((JTextField) jc).addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							JTextField j = (JTextField)e.getSource();
+							DataControl.DatumDef datum = parTable.get(j.getName());
+							if(datum != null) {
+								String err = datum.setValue(j.getText());
+								if(err != null) 
+									JOptionPane.showMessageDialog(null, err, "Error", JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					});
+									
+					widgets[i].setGui(jc);
+					parTable.put(widgets[i].getName(), widgets[i]);	
+					break;
+					//
+				default:
+					break;
+			}
+		}
+		
+		//
+		// now group the widgets into 2 columns
+		//
+		
 		GroupLayout layout = new GroupLayout(parent);
 		parent.setLayout(layout);
 
-		// gaps between components and
-		// gaps between container and components touching the edge of it
+		// set gaps between components
 		layout.setAutoCreateGaps(true);
+		// set gaps between container and components touching the edge of it
 		layout.setAutoCreateContainerGaps(true);
 
 		// Create one sequential group for horizontal axis, and one for vertical
 		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
 
-		// The sequential group in turn contains two parallel groups.
-		// One parallel group contains the labels, the other the text fields.
-		// Putting the labels in a parallel group along the horizontal axis
-		// positions them at the same x location.
-		// Variable indentation is used to reinforce the level of grouping.
-		//
-		// The sequential group contains two parallel groups that align
-		// the contents along the baseline. The first parallel group contains
-		// the first label and text field, and the second parallel group
-		// contains
-		// the second label and text field. By using a sequential group
-		// the labels and text fields are positioned vertically after one
-		// another.
-
+		// hGroup contains two parallel groups, one for labels and one for text fields
 		GroupLayout.ParallelGroup h1 = layout.createParallelGroup();
 		GroupLayout.ParallelGroup h2 = layout.createParallelGroup();
-
-		// create the control widgets
-	
-		ArrayList<JComponent> jclist = new ArrayList<JComponent>();
-		JComponent jc;
-		for (int i = 0; i < widgets.length; i++) {
-			switch (widgets[i].type) {
-				case BUTTON:
-					jc = new JButton(widgets[i].name);
-					jc.setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					jc.setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					jclist.add(jc);
-					break;
-				case LABEL:
-					jc = new JLabel(widgets[i].name);
-					jc.setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					jc.setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					jclist.add(jc);
-					// fall through. label always followed by its text field
-				case TFIELD:
-					jc = new JTextField();
-					jc.setName(widgets[i].name);
-					((JTextField) jc).addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							// TODO
-						}
-					});
-					((JTextField) jc).setMinimumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					((JTextField) jc).setMaximumSize(new Dimension(WIDGET_WIDTH, WIDGET_HEIGHT));
-					((JTextField) jc).setColumns(10);
-					
-					// keep track of text field for auto edit internally
-					jclist.add(jc);
-					parTable.put(widgets[i].name, jc);
-					break;
-				default:
-					break;
-			}
-		}
 		
-			
-		// group every 2 control widgets
+		// group widgets into 2 columns
 		for (int i = 1; i < jclist.size(); i += 2) {
 		
 			h1 = h1.addComponent(jclist.get(i-1));
 			h2 = h2.addComponent(jclist.get(i));
+			
+			// add a parallel group that aligns a label and a text field along the baseline
 			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE)
 			      .addComponent(jclist.get(i-1)).addComponent(jclist.get(i)));
 		}
